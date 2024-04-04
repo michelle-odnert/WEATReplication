4c3aeba1d1374c105a0360bf00b132d374245f6f
Michelle Odnert <modnert@uw.edu>
2024-04-04 08:30:08 -0700

    Update word lists

--- target-attribute-convert.py
+++ target-attribute-convert.py
@@ -17,5 +17,42 @@ def create_weat_dict(weat_dir, output_path):
 
         if os.path.abspath(output_path):
             continue
-
         
+    # remove underscore and file extension
+    key = os.path.splitext(data_name)[0].replace('_', '')
+    values = create_word_list(path)
+    weat_dict[key] = {'words': values}
+
+    weat_dict['flowers_vs_insects_pleasant_vs_unpleasant'] = {
+        'method': 'weat',
+        'X_key': 'flowers',
+        'Y_key': 'insects',
+        'A_key': 'pleasant',
+        'B_key': 'unpleasant',
+        'targets': 'flowers vs insects',
+        'attributes': 'pleasant vs unpleasant',
+    }
+
+    
+    # target: instruments vs. weapons
+    # attributes: pleasant vs. unpleasant
+    weat_dict['instruments_vs_weapons_pleasant_vs_unpleasant'] = {
+        'method': 'weat',
+        'X_key': 'instruments',
+        'Y_key': 'weapons',
+        'A_key': 'pleasant',
+        'B_key': 'unpleasant',
+        'targets': 'instruments vs weapons',
+        'attributes': 'pleasant vs unpleasant',
+    }
+
+    # European-American vs. African-American names
+    weat_dict['EuropeanAmerican_vs_AfricanAmerican_pleasant_vs_unpleasant'] = {
+        'method': 'weat',
+        'X_key': 'EuropeanAmerican',
+        'Y_key': 'AfricanAmerican',
+        'A_key': 'pleasant',
+        'B_key': 'unpleasant',
+        'targets': 'European-American names vs African-American names',
+        'attributes': 'pleasant vs unpleasant',
+    }
